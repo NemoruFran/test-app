7543216e7789cc3c548b39150b6881150f42783d
Fran <francisco.sanchez@derivco.co.uk>
2022-07-20 16:07:07 +0200

    first commit of angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //also template and writing html inline works
   styleUrls: ['./app.component.css']
+  //also styles and writing css inside works 
 })
 export class AppComponent {
-  title = 'test-app';
 }
