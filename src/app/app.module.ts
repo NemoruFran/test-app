d3b8a1030cae1033f3335e5e66160c73ef2c55a2
Fran <francisco.sanchez@derivco.co.uk>
2022-07-21 17:03:13 +0200

    Assignment 1 from course done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SucessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SucessAlertComponent
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //esto en realidad es un selector css. Si pones [app-servers] entonces poniendo <div app-servers> como atributo hara que 
+  //Angular reconozca ese div como el componente al cual se le ha de inyectar el html y el css especificados en este componente.
+  //tambien funciona poniendolo como clase, rollo selector: '.app-servers' y <div class='app-servers'> 
+  //el unico caso en el que no funciona es seleccionando por id.
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
