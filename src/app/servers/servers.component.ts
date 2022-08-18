7cec080ee241e73b31e5e31e828a9bb12156b21a
Fran <francisco.sanchez@derivco.co.uk>
2022-08-18 20:08:35 +0200

    added ngfor attribute directive (like ngIf directive but to replicate a component)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus="No server was created!"
   serverName = "test server :D";
   serverCreated = false;
+  servers = ["Testserver","Testserver 2"]
   
   constructor() {
     /*setTimeout(() => {
@@ -26,7 +27,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    //this.serverCreationStatus = "Server was created! Name is" + this.serverName;
+    this.serverCreationStatus = "Server was created! Name is" + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
 
